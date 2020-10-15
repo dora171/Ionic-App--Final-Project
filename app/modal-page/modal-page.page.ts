91a66906c3cd8fbdd52817e0d136c54f69fa5744
dora171 <72926094+dora171@users.noreply.github.com>
2020-10-15 19:29:06 +0200

    Add files via upload

---
+++ app/modal-page/modal-page.page.ts
@@ -0,0 +1,17 @@
+import { Component, OnInit } from '@angular/core';
+import { NavParams } from '@ionic/angular';
+
+
+@Component({
+  selector: 'app-modal-page',
+  templateUrl: './modal-page.page.html',
+  styleUrls: ['./modal-page.page.scss'],
+})
+export class ModalPagePage implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
